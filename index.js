473c6f091d86c45ce8bd0f3a906c7b4801f184cf
vyslbyrx <58915055+vyslbyrx@users.noreply.github.com>
2022-07-22 18:40:40 +0300

    kategori duzenlemeleri

--- index.js
+++ index.js
@@ -27,7 +27,6 @@ fetch("https://live.runtext.de/multi_search?q=", {
 
 function CategoriesList(CatResult) {
   let data = CatResult.results[0].hits;
-  console.log(data)
   let catList = data[0].document.AvailableCategories;
   let menuBox = document.getElementById("menu");
   if(catList.length > 0) {
@@ -44,86 +43,78 @@ function CategoriesList(CatResult) {
     })
   }
   catListItems(catList)
-  //CatList2(catList)
 }
-
+const categoryListBox = document.querySelector("#categoryList");
 function catListItems(catList) {
   document.querySelectorAll(".category-list").forEach(element => {
     element.addEventListener("click", function () {
-      element.classList.add("nav-url")
+      element.classList.add("nav-url");
       let dataCatName = element.dataset.catname;
-      if(element.classList.contains("active")) {
-        element.classList.remove("active");
-        setTimeout(function() {
-          element.nextElementSibling.remove();
-        },600)
-      } else {
-        element.classList.add("active");
-        let subList = catList.filter(sub => sub.Text.includes(dataCatName))
-        let tabIndex = element.dataset.index;
-        if(tabIndex === "0") {
-          let list = document.createElement("ul");
-          list.classList.add("sublist");
-          element.parentElement.append(list);
-          subList.map(subItem => {
-            let subArr = subItem.Text.split(">>");
-            if(subArr.length > 1) {
-              element.nextElementSibling.innerHTML += `<li class="sub-item"><a class="category-list-sub-item" data-index="${Number(tabIndex)+1}">${subArr[Number(tabIndex)+1]}</a></li>`
-            }
-          })
-        }
+      categoryListBox.innerHTML = "";
+      element.classList.add("active");
+      let subList = catList.filter(sub => sub.Text.includes(dataCatName))
+      let tabIndex = element.dataset.index;
+      let slider = `<div class="container direction-rtl">
+      <div class="card mb-3">
+        <div class="card-body">
+          <div class="row g-3">
+          </div>
+        </div>
+      </div>
+    </div>`;
+      categoryListBox.innerHTML += slider;
+      if(tabIndex === "0") {
+        subList.map(subItem => {
+          let subArr = subItem.Text.split(">>");
+          if(subArr.length > 1) {
+            document.querySelector("#categoryList .row").innerHTML += `<div class="col-4 category-sub-item" >
+                                              <div class="feature-card mx-auto text-center">
+                                                    <div class="card mx-auto bg-gray" data-index="${Number(tabIndex)+1}" data-name="${subArr[Number(tabIndex)+1]}"></div>
+                                                    <p class="mb-0">${subArr[Number(tabIndex)+1]}</p>
+                                                 </div>
+                                          </div>`
+          }
+        })
       }
     })
   })
 }
 
-const menu = document.querySelector("#menu");
+const menu = document.querySelector("#categoryList");
 const pHandler = e => {
-  if (e.target.tagName === 'A') { //This line also part of the event delegation
+  if (e.target.classList.contains("card")) {
     let itemDataIndex = e.target.dataset.index;
+    let itemCategoryName = e.target.dataset.name;
+    subCategories(itemDataIndex,itemCategoryName);
   }
 };
 menu.addEventListener("click", pHandler)
 
-function CatList2(catList) {
-  document.querySelectorAll(".category-list").forEach(element => {
-    element.addEventListener("click", function () {
-      element.classList.add("nav-url")
-      let dataCatName = element.dataset.catname;
-        element.classList.add("active");
-        let subList = catList.filter(sub => sub.Text.includes(dataCatName))
-        let tabIndex = element.dataset.index;
-        if(Number(tabIndex) > 1) {
-          let list = document.createElement("ul");
-          list.classList.add("sublist");
-          element.parentElement.append(list);
-          console.log(element.dataset.index)
-          tabIndex = element.parentElement.previousElementSibling.dataset.index;
-          subList.map(subItem => {
-            console.log(subItem)
-            let subArr = subItem.Text.split(">>");
-            if(subArr.length > 2) {
-              element.nextElementSibling.innerHTML += `<li class="sub-item"><a class="category-list" data-index="${Number(tabIndex)+1}">${subArr[Number(tabIndex)+1]}</a></li>`
-            }
-          })
-        }
-      
-    })
+function subCategories(itemDataIndex,itemCategoryName) {
+  fetch("https://live.runtext.de/multi_search?q=", {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+      "X-TYPESENSE-API-KEY": apiKey
+    },
+    body: JSON.stringify(body)
   })
-  
-}
-
-function CategoryDescription() {
-  if(indexNum !== -1) {
-    let catDesc = data[indexNum].document.Description;
-    if(catDesc !== null && catDesc !== undefined) {
-      cat = `<span class="desc" style="display: block;padding: 2px;border: 1px solid #c4c4c420;background-color: #00000020">${data[indexNum].document.Description}</span>`
-      menuBox.innerHTML += `<li data-catName="${item.Text}">${item.Text} ${cat}</li>`;
-    } else {
-      menuBox.innerHTML += `<li data-catName="${item.Text}">${item.Text}</li>`
-    }
-  }
-}
-function CatListSubCategory(CatName) {
-
+    .then(res => res.json()
+    ).then(data => {
+      let categories = data.results[0].hits;
+      let catList = categories[0].document.AvailableCategories;
+      document.querySelector("#categoryList .row").innerHTML = "";
+      catList.map(cat => {
+        let subArr = cat.Text.split(">>");
+        if(subArr[Number(itemDataIndex)] === itemCategoryName) {
+          document.querySelector("#categoryList .row").innerHTML += `<div class="col-4 category-sub-item" >
+                                                <div class="feature-card mx-auto text-center">
+                                                      <div class="card mx-auto bg-gray" data-index="${Number(itemDataIndex)+1}" data-name="${subArr[Number(itemDataIndex)+1]}"></div>
+                                                      <p class="mb-0">${subArr[Number(itemDataIndex)+1]}</p>
+                                                   </div>
+                                            </div>`
+        }
+      })
+    
+  })
 }
